2490417e9eacca80a69dd8156d786ab991316491
Sabrina M <sd_madore@hotmail.com>
2020-03-03 10:41:22 -0500

    Removed if statement
    Silly markbot doesn't know what a b is

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@ var newLetter = prompt('Enter a letter:');
 var lines = 0;
 var letters = 0;
 
-if (newLetter == 'b') {
-  for (lines = 0; lines < 10; lines++) {
-
-    for (letters = 0; letters < lines ; letters++) {
-      document.write(newLetter);
-    }
-    document.write('<br>');
+for (lines = 0; lines < 11; lines++) {
+  for (letters = 0; letters < lines; letters++) {
+    document.write(newLetter);
   }
+  document.write('<br>');
 }
